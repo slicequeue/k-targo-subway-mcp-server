bf718024c0820d04ddbf963a503efc68ec9a5191
slicequeue <slicequeue@gmail.com>
2025-08-02 19:10:51 +0900

    refactor: 사용하지 않는 도구들 제거 (add, multiply)

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,19 +1,13 @@
-import { addTool } from '@/tools/add';
-import { multiplyTool } from '@/tools/multiply';
 import { searchSubwayStationTool, getSubwayTimetableTool, getAllSubwayTimetablesTool } from '@/tools/subway';
 import { ToolsContainer } from '@/tools/types';
 
 export const tools: ToolsContainer = {
-  add: addTool,
-  multiply: multiplyTool,
   search_subway_station: searchSubwayStationTool,
   get_subway_timetable: getSubwayTimetableTool,
   get_all_subway_timetables: getAllSubwayTimetablesTool
 };
 
 export { 
-  addTool, 
-  multiplyTool,
   searchSubwayStationTool,
   getSubwayTimetableTool,
   getAllSubwayTimetablesTool
